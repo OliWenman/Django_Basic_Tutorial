a535433935b83f1e15ca42072439e5b0fce057b1
unknown <oliwenman1@gmail.com>
2020-01-30 16:13:52 +0000

    Tutorial 3 complete

--- Polls_App/urls.py
+++ Polls_App/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', 
-					views.index, 
-					name = 'index'),
+app_name = 'Polls_App'
+urlpatterns = [
+	# ex: /Polls_App/
+	path('', views.index, name = 'index'),
+
+	# ex: /Polls_App/5/
+	path('<int:question_id>/', views.detail, name = 'detail'),
+
+	# ex: /Polls_App/5/results/
+	path('<int:question_id>/results/', views.results, name = 'results'),
+
+	# ex: /Polls_App/5/vote/
+	path('<int:question_id>/vote/', views.vote, name = 'vote'),
 ]
 
--- Polls_App/views.py
+++ Polls_App/views.py
@@ -1,6 +1,59 @@
-from django.http import HttpResponse
+"""
+A view is a type of web page in your application that generally 
+serves a specific functoon has a specific template.
+
+Poll app
+- Question "index" page, displays latest questions
+- Question "detail" page, displays a question text, with
+  no results but with a form to vote in.
+- Question "results page", display results for a poll
+- "Vote action - handles voting for  particular choice 
+  in a particular question.
+"""
+
+#from django.http import HttpResponse
+#from django.template import loader
+from django.shortcuts import render, get_object_or_404
+#from django.http import Http404
+
+from .models import Question
 
 def index(request):
-	return HttpResponse("Hello World")
 
-# Create your views here.
+	latest_question_list = Question.objects.order_by('-published_date')[:5]
+
+	#template = loader.get_template('Polls_App/index.html')
+
+	context = {'latest_question_list': latest_question_list,}
+
+	#output = ', '.join([q.question_text for q in latest_question_list])
+	
+	#return HttpResponse(template.render(context, request))
+
+	return render(request, 'Polls_App/index.html', context)
+
+
+def detail(request, question_id):
+	"""
+	try:
+		question = Question.objects.get(pk = question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	"""
+
+	question = get_object_or_404(Question, pk = question_id)
+	#return HttpResponse("You're looking at question %s." % question_id)
+	return render(request, 'Polls_App/detail.html', {'question' : question}) 
+
+	
+
+
+def	results(request, question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(repsonse % question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("Your're voting on question %s." % question_id)
+
+
